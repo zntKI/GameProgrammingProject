da25feb9a94e1883e9e6ce6c16912841ee813db9
zntKI <kaloyanivanov@hotmail.com>
2023-12-12 15:34:29 +0100

    Created player class
    Created a basic player class with an example sprite

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -3,26 +3,15 @@ using GXPEngine;                                // GXPEngine contains the engine
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
+	public MyGame() : base(800, 800, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
-
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
-		Console.WriteLine("MyGame initialized");
+		Player player = new Player(width / 2, height / 2); ;
+		AddChild(player);
 	}
 
 	// For every game object, Update is called every frame, by the engine:
 	void Update() {
-		// Empty
+		
 	}
 
 	static void Main()                          // Main() is the first method that's called when the program is run
---
+++ GXPEngine/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using GXPEngine;
+public class Player : Sprite
+{
+    public Player(float x, float y) : base("square.png", false, true)
+    {
+        this.SetOrigin(width / 2, height / 2);
+        this.SetXY(x, y);
+    }
+
+
+}
